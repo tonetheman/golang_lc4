64c5e639f926b1ce34d45e57f59f82771b7cf4ca
tonetheman <tonetheman@gmail.com>
2018-03-14 22:58:53 -0500

    init and started translate

--- prog.go
+++ prog.go
@@ -4,6 +4,9 @@ import "fmt"
 
 type m [6][6]int
 
+// this needs to have 36 values and
+// should be a permutation
+// TODO: fix this
 func makekey(_s string) string {
 	var s string
 	for i := 0; i < 35; i++ {
@@ -12,14 +15,32 @@ func makekey(_s string) string {
 	return s[0:36]
 }
 
-func main() {
+func (mat *m) initState(key string) {
+	for k := 0; k < 36; k++ {
+		var row int = k / 6
+		var col int = k % 6
+		mat[row][col] = int(key[k])
+	}
+}
 
+func string_to_base35array(s string) []byte {
+	var tmp []byte = make([]byte, len(s))
+	for idx := range s {
+		if s[idx] == "0" {
+			tmp[idx] = 0
+		}
+		if s[idx] == "1" {
+			tmp[idx] = 1
+		}
+	}
+	return tmp
+}
+
+func main() {
 	var key = makekey("tony")
 	fmt.Println("len of key is", len(key))
 	var mat m
 	fmt.Println(mat)
-	for k := 0; k < 35; k++ {
-		mat[k/6][k%6] = int(key[k])
-	}
+	mat.initState(key)
 	fmt.Println(mat)
 }
